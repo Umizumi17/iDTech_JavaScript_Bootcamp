874669b91d4240c8002cd206fed5529d360f60ca
Umizumi17 <shopulatovUmi@gmail.com>
2022-01-08 16:40:28 -0500

    even more projects uploading

---
+++ story_time.js
@@ -0,0 +1,18 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let time = 
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
