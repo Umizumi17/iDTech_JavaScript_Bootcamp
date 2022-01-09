53be8dffa59e87870f021d4443a047d12d53e8fa
Umizumi17 <shopulatovUmi@gmail.com>
2022-01-08 19:43:35 -0500

    final assignenments upload

--- story_time.js
+++ story_time.js
@@ -1,18 +1,27 @@
 // TODO: Create 4-String variables to introduce develop your story.
 // YOUR CODE GOES HERE
-let time = 
-
+let greeting = "welcome to this cursed castle";
+let characterName = "Bob";
+let story = "is going to spend the night in the cursed castle";
+let finish = "he is terrified";
 // TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
 // YOUR CODE GOES HERE
-
+var numberofItems = 5;
+var age = 24;
+var year = 1500;
 
 // TODO: Create 1-Array variable to show a collection of items your character might have.
 // YOUR CODE GOES HERE
-
+let items = ["tent", "food", "knife", "candle"];
 
 // TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
 // YOUR CODE GOES HERE
-
+let isTerrified = true;
 
 // TODO: Print your story to the console.
 // YOUR CODE GOES HERE
+console.log(greeting + ". This story is about " + characterName + " the great, he is a knight, and he has to do a  bravery challenge of spending the night in a cursed catle. ");
+console.log("For his ceremony he is " + age + " years old." + characterName + " " + finish);
+console.log (" Bob has brought " + numberofItems + " things with him to the cursed castle. ")
+console.log ("In his bag, there is " + items);
+console.log(". His plan was to stay up all night, but he fell asleep at midnight. He got woken up by strange noices and " + finish + ". He though it was a monster so he puller out his knife to protect himself, but turned out to be a bird. He ended up staying up the rest of the nigth with the bird");
